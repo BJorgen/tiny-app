19fca22f0692ff7da2db7d28e06b75affcdc248d
Britta Jorgenson <bljorgen@gmail.com>
2019-05-07 23:44:11 +0000

    Added urls_index.ejs and linked it to the /urls request render response.

--- express_server.js
+++ express_server.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var app = express();
-var PORT = 8080;
+const express = require('express');
+const app = express();
+const PORT = 8080;
 
 app.set('view engine', 'ejs');
 
-var urlDatabase = {
+const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
 };
@@ -19,6 +19,11 @@ app.get('/urls.json', (req, res) => {
 
 app.get('/hello', (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n")
+});
+
+app.get('/urls', (req, res) => {
+    let templateVars = { urls: urlDatabase };
+    res.render('urls_index', templateVars);
 });
 
 app.listen(PORT, () => {
