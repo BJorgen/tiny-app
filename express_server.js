5d04f04fe96c09868192ab39d2e2e96a6f1bbd97
Britta Jorgenson <britta.jorgenson@gmail.com>
2019-05-08 16:03:25 -0600

    Passed username to all views and displayed in the header under the login.

--- express_server.js
+++ express_server.js
@@ -30,19 +30,6 @@ app.get('/hello', (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n")
 });
 
-app.get('/urls', (req, res) => {
-    let templateVars = { urls: urlDatabase };
-    res.render('urls_index', templateVars);
-});
-
-app.get('/urls/new', (req, res) => {
-    res.render('urls_new')
-});
-
-app.get('/urls/:shortURL', (req, res) => {
-    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-    res.render('urls_show', templateVars);
-});
 
 function generateRandomString() {
     let randomString = '';
@@ -52,6 +39,35 @@ function generateRandomString() {
     }
     return randomString;
 }
+
+
+
+app.get('/urls', (req, res) => {
+    let templateVars = {
+        username: req.cookies["username"],
+        urls: urlDatabase
+    };
+    res.render('urls_index', templateVars);
+});
+
+app.get('/urls/new', (req, res) => {
+    let templateVars = {
+        username: req.cookies["username"]
+    };
+    res.render('urls_new', templateVars)
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+    let templateVars = {
+        username: req.cookies["username"],
+        shortURL: req.params.shortURL, 
+        longURL: urlDatabase[req.params.shortURL]
+    };
+    res.render('urls_show', templateVars);
+});
+
+
+
 
 app.post('/urls', (req, res) => {
     shortURL = generateRandomString();
@@ -78,6 +94,8 @@ app.get("/u/:shortURL", (req, res) => {
     const longURL = urlDatabase[req.params.shortURL];
     res.redirect(longURL);
 });
+
+
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
